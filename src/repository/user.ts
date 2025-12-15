6ebf8043dbbfe60bbfafc59683646c20a07c9659
taro.tanaka <taro.tanaka@example.com>
2025-12-15 12:01:32 +0900

    ユーザーリポジトリにgetMuseumsメソッドを追加

--- src/repository/user.ts
+++ src/repository/user.ts
@@ -4,6 +4,10 @@ import {
     type User,
     type UserId
 } from "@/schema/user.js"
+import {
+    MuseumListSchema,
+    type MuseumList,
+} from "@/schema/museum.js"
 
 export class UserRepository {
 
@@ -30,4 +34,15 @@ export class UserRepository {
         if (!response.ok) { throw new Error(response.statusText) }
         return UserSchema.parse(await response.json())
     }
+    
+        async getMuseums(): Promise<MuseumList> {
+            const url = `/users/me/museums`
+            const response = await this.fetch(url, {
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' },
+                credentials: 'include',
+            })
+            if (!response.ok) { throw new Error(response.statusText) }
+            return MuseumListSchema.parse(await response.json())
+        }
 }
