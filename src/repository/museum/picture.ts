da1a4b807012b117ea63dff0e26c760997749145
YasuiUnagi <yasuiunagi276951438@gmail.com>
2025-12-18 23:22:31 +0900

    deleteを修正

--- src/repository/museum/picture.ts
+++ src/repository/museum/picture.ts
@@ -52,7 +52,9 @@ export class PictureRepository {
         const url = `${this.baseURL}/${id}`
         const response = await fetch(url, {
             method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' },
             credentials: 'include',
+            body: JSON.stringify({})
         })
         if (!response.ok) { throw new Error(response.statusText) }
     }
