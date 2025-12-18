b57c76346f644a2d1f24ca10ad3c51e5b8b5461d
taro.tanaka <taro.tanaka@example.com>
2025-12-18 23:17:14 +0900

    feat: EventArticleRepositoryを追加し、イベントに関連する記事の取得と更新機能を実装

---
+++ src/repository/museum/event-article.ts
@@ -0,0 +1,46 @@
+import type { MuseumId } from '@/schema/museum.js'
+import {
+    EventSchema,
+    EventListSchema,
+    type Event,
+    type EventId,
+    type EventList,
+} from '@/schema/event.js'
+import {
+    ArticleListSchema,
+    type Article,
+    type ArticleId,
+    type ArticleList,
+} from '@/schema/article.js'
+
+export class EventArticleRepository {
+    
+    readonly baseURL: string
+
+    constructor(baseURL: string, museumId: MuseumId, eventId: EventId) {
+        this.baseURL = `${baseURL}/museums/${museumId}/events/${eventId}/articles`
+    }
+
+    async get(): Promise<ArticleList> {
+        const url = `${this.baseURL}`
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'include',
+        })
+        if (!response.ok) { throw new Error(response.statusText) }
+        return ArticleListSchema.parse(await response.json())
+    }
+
+    async put(articleIds: ArticleList): Promise<ArticleList> {
+        const url = `${this.baseURL}`
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'include',
+            body: JSON.stringify(articleIds),
+        })
+        if (!response.ok) { throw new Error(response.statusText) }
+        return ArticleListSchema.parse(await response.json())
+    }
+}
