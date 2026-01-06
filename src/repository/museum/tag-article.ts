48f78cde9f9834aa26c6481ef82686e944860642
taro.tanaka <taro.tanaka@example.com>
2026-01-06 10:15:45 +0900

    feat(museum): add TagArticleRepository to manage articles by tag

---
+++ src/repository/museum/tag-article.ts
@@ -0,0 +1,26 @@
+import type { MuseumId } from "@/schema/museum.js";
+import type { TagId } from "@/schema/tag.js";
+import { 
+    ArticleIdListSchema, 
+    type ArticleList,
+    type ArticleIdList,
+    ArticleListSchema
+} from "@/schema/article.js";
+
+export class TagArticleRepository {
+    readonly baseURL: string
+    constructor(baseURL: string, museumId: MuseumId, tagId: TagId) {
+        this.baseURL = `${baseURL}/museums/${museumId}/tags/${tagId}/articles`
+    }
+
+    async get(): Promise<ArticleList> {
+        const url = `${this.baseURL}`
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'include',
+        })
+        if (!response.ok) { throw new Error(response.statusText) }
+        return ArticleListSchema.parse(await response.json())
+    }
+}
